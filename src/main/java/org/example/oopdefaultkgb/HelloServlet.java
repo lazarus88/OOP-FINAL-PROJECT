18b3a85e7e8c8d98771da122b12d29392bf64caa
kkerd20 <kkerd20@freeuni.edu.ge>
2024-07-02 00:53:05 +0400

    config servlet

--- src/main/java/org/example/oopdefaultkgb/HelloServlet.java
+++ src/main/java/org/example/oopdefaultkgb/HelloServlet.java
@@ -1,9 +1,12 @@
 package org.example.oopdefaultkgb;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
