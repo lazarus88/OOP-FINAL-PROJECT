6e2784ee4d6b0e1479e616911be78d184b2b03ef
kkerd20 <kkerd20@freeuni.edu.ge>
2024-07-05 03:01:07 +0400

    database tabulation fix

--- DataBase.sql
+++ DataBase.sql
@@ -3,76 +3,70 @@ CREATE SCHEMA `kgb` ;
 USe kgb;
 
 CREATE TABLE `User` (`Id` bigInt NOT NULL AUTO_INCREMENT,
-                     `FullName` varchar(100),
-                     `UserName` varchar(100),
-                     `HashPassword` varchar(100),
-                     `Status` varchar(100),
-                     `CreatedAt` Date,
-                     `UpdatedAt` Date,
-                     `UpdateAdminId` bigInt,
-                     `Role` varchar(100),
+                    `FullName` varchar(100),
+                    `UserName` varchar(100),
+                    `HashPassword` varchar(100),
+                    `Status` varchar(100),
+                    `CreatedAt` Date,
+                    `UpdatedAt` Date,
+                    `UpdateAdminId` bigInt,
+                    `Role` varchar(100),
                      PRIMARY KEY (`Id`)
 );
 
-CREATE TABLE `UserAchivement` (
-                                  `Id` bigInt NOT NULL AUTO_INCREMENT,
-                                  `UserId` bigInt,
-                                  `AchivementId` int,
-                                  `AchivedAt` Date,
-                                  PRIMARY KEY (`Id`)
+CREATE TABLE `UserAchivement` (`Id` bigInt NOT NULL AUTO_INCREMENT,
+                               `UserId` bigInt,
+                               `AchivementId` int,
+                               `AchivedAt` Date,
+                               PRIMARY KEY (`Id`)
 );
 
 
-CREATE TABLE `UserQuizHistory` (
-                                   `Id` bigInt NOT NULL AUTO_INCREMENT,
-                                   `UserId` bigInt,
-                                   `QuizId` int,
-                                   `TookAt` Date,
-                                   `Duration` bigInt,
-                                   `Score` int,
-                                   `Status` varchar(100),
-                                   `IsPractice` bit,
-                                   PRIMARY KEY (`Id`)
+CREATE TABLE `UserQuizHistory` (`Id` bigInt NOT NULL AUTO_INCREMENT,
+                               `UserId` bigInt,
+                               `QuizId` int,
+                               `TookAt` Date,
+                               `Duration` bigInt,
+                               `Score` int,
+                               `Status` varchar(100),
+                               `IsPractice` bit,
+                               PRIMARY KEY (`Id`)
 );
 
-CREATE TABLE `Mail` (
-                        `Id` bigInt NOT NULL AUTO_INCREMENT,
-                        `SenderUserId` bigInt,
-                        `ReciverUserId` bigInt,
-                        `Message` varchar(100),
-                        `MailTypeId` int,
-                        `CreatedAt` Date,
-                        `Status` varchar(100),
-                        PRIMARY KEY (`Id`)
+CREATE TABLE `Mail` (`Id` bigInt NOT NULL AUTO_INCREMENT,
+                    `SenderUserId` bigInt,
+                    `ReciverUserId` bigInt,
+                    `Message` varchar(100),
+                    `MailTypeId` int,
+                    `CreatedAt` Date,
+                    `Status` varchar(100),
+                    PRIMARY KEY (`Id`)
 );
 
-CREATE TABLE `Quiz` (
-                        `Id` bigInt NOT NULL AUTO_INCREMENT,
-                        `CreatorUserId` bigInt,
-                        `IsRandom` bit,
-                        `IsOneVsMultiple` bit,
-                        `IsImmidiatle` bit,
-                        `IsPracticeEnable` bit,
-                        `QuizTypeId` int,
-                        `Status` varchar(100),
-                        PRIMARY KEY (`Id`)
+CREATE TABLE `Quiz` (`Id` bigInt NOT NULL AUTO_INCREMENT,
+                    `CreatorUserId` bigInt,
+                    `IsRandom` bit,
+                    `IsOneVsMultiple` bit,
+                    `IsImmidiatle` bit,
+                    `IsPracticeEnable` bit,
+                    `QuizTypeId` int,
+                    `Status` varchar(100),
+                    PRIMARY KEY (`Id`)
 );
 
-CREATE TABLE `Achivement` (
-                              `Id` bigInt NOT NULL AUTO_INCREMENT,
-                              `UserId` bigInt,
-                              `AchivementId` int,
-                              `AchievedAt` Date,
-                              PRIMARY KEY (`Id`)
+CREATE TABLE `Achivement` (`Id` bigInt NOT NULL AUTO_INCREMENT,
+                          `UserId` bigInt,
+                          `AchivementId` int,
+                          `AchievedAt` Date,
+                          PRIMARY KEY (`Id`)
 );
 
-CREATE TABLE `Friend` (
-                          `Id` bigInt NOT NULL AUTO_INCREMENT,
-                          `senderUserId` bigInt,
-                          `ReciverUserId` bigInt,
-                          `invitedAt` Date,
-                          `status` varchar(100),
-                              PRIMARY KEY (`Id`)
+CREATE TABLE `Friend` (`Id` bigInt NOT NULL AUTO_INCREMENT,
+                      `senderUserId` bigInt,
+                      `ReciverUserId` bigInt,
+                      `invitedAt` Date,
+                      `status` varchar(100),
+                      PRIMARY KEY (`Id`)
 );
 
 
