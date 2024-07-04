9c34bd5b8ae47db1d3b3c1a3020dbca094a90bcc
Natula88 <ntati20@freeuni.edu.ge>
2024-07-04 22:51:27 +0400

    .

---
+++ src/main/java/org/example/oopdefaultkgb/EntityDTO/Mail.java
@@ -0,0 +1,17 @@
+package org.example.oopdefaultkgb.EntityDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class Mail {
+    public int id;
+    public int SenderUserId;
+    public int ReceiverUserId;
+    public String message;
+    public int MailTypeId;
+    public Date CreatedAt;
+}
