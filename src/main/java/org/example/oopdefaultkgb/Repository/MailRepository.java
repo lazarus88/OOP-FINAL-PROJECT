a10e3e91e0644c1d50abbd7b35e6113bd9b5da80
Natula88 <ntati20@freeuni.edu.ge>
2024-07-05 19:16:54 +0400

    end of MailRepository so far

--- src/main/java/org/example/oopdefaultkgb/Repository/MailRepository.java
+++ src/main/java/org/example/oopdefaultkgb/Repository/MailRepository.java
@@ -1,6 +1,8 @@
 package org.example.oopdefaultkgb.Repository;
 
+import org.example.oopdefaultkgb.EntityDTO.Friend;
 import org.example.oopdefaultkgb.EntityDTO.Mail;
+import org.example.oopdefaultkgb.EntityDTO.User;
 import org.example.oopdefaultkgb.Interface.Repository.IMailRepository;
 
 import java.sql.Connection;
@@ -8,18 +10,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class MailRepository extends BaseRepository implements IMailRepository {
-
-    private final String CHALLENGE_REQUEST_SEND = " თქვენ გაუგზავნეთ {%s} ქვიზის Challenge {%s} - ს";
-    private final String CHALLENGE_REQUEST_RECEIVE = " {%s} - მა გამომიგზავნათ {%s} ქვიზის Challenge";
     private final int FRIEND_REQUEST_ID = 1;
     private final int CHALLENGE_REQUEST_ID = 2;
     private final int NOTE_ID = 3;
 
-    private final String SENT = "sent";
+    private final String SENT = "SENT";
+    private final String ACCEPTED = "ACCEPTED";
+    private final String REJECTED = "REJECTED";
 
 
     public MailRepository() throws SQLException {
@@ -53,39 +55,45 @@ public class MailRepository extends BaseRepository implements IMailRepository {
     @Override
     public boolean acceptFriendRequest(int userId, int userIdFrom) throws SQLException {
         Statement statement =ConnectionString.createStatement();
-    String query = String.format("UPDATE Mail SET Status = 'ACCEPTED' WHERE (SenderUserId = %d AND ReceiverUserId = %d AND MailTypeId = %d)", userIdFrom, userId, FRIEND_REQUEST_ID);
+    String query = String.format("UPDATE Mail SET Status = '%s' WHERE (SenderUserId = %d AND ReceiverUserId = %d AND MailTypeId = %d AND Status = %s)", ACCEPTED,userIdFrom, userId, FRIEND_REQUEST_ID, SENT);
         return statement.execute(query);
 }
 
     @Override
-    public boolean acceptChallengeRequest(int userId, int userIdFrom, int quizId) {
-        return false;
+    public boolean acceptChallengeRequest(int userId, int userIdFrom, String quizName) throws SQLException {
+        Statement statement =ConnectionString.createStatement();
+        String query = String.format("UPDATE Mail SET Status = '%s' WHERE (SenderUserId = %d AND ReceiverUserId = %d AND MailTypeId = %d AND Status = %s AND message = %s)", ACCEPTED, userIdFrom, userId, CHALLENGE_REQUEST_ID, SENT, quizName);
+        return statement.execute(query);
     }
 
     @Override
-    public boolean rejectFriendRequest(int userId, int userIdFrom) {
-        return false;
+    public boolean rejectFriendRequest(int userId, int userIdFrom) throws SQLException {
+        Statement statement =ConnectionString.createStatement();
+        String query = String.format("UPDATE Mail SET Status = '%s' WHERE (SenderUserId = %d AND ReceiverUserId = %d AND MailTypeId = %d AND Status = %s)", REJECTED,userIdFrom, userId, FRIEND_REQUEST_ID, SENT);
+        return statement.execute(query);
     }
 
     @Override
-    public boolean rejectChallengeRequest(int userId, int userIdFrom, int quizId) {
-        return false;
+    public boolean rejectChallengeRequest(int userId, int userIdFrom, String quizName) throws SQLException {
+        Statement statement =ConnectionString.createStatement();
+        String query = String.format("UPDATE Mail SET Status = '%s' WHERE (SenderUserId = %d AND ReceiverUserId = %d AND MailTypeId = %d AND Status = %s AND message = %s)",REJECTED, userIdFrom, userId, CHALLENGE_REQUEST_ID, SENT, quizName);
+        return statement.execute(query);
     }
 
     @Override
-    public List<Mail> getFriendRequests(int userId) {
-        return null;
+    public List<Mail> getMails(int userId, int mailTypeId) throws SQLException {
+        var MailList = new ArrayList<Mail>();
+        Statement statement =ConnectionString.createStatement();
+        String query = String.format("SELECT * FROM Mail WHERE (ReceiverUserId = %d AND MailTypeId = %d AND Status = %s)", userId, mailTypeId, SENT);
+        ResultSet res = statement.executeQuery(query);
+        if(!res.next()) return null;
+        while(res.next())
+              MailList.add(new Mail(
+                      res.getInt(1), res.getInt(2),
+                      res.getInt(3), res.getString(4),
+                      res.getInt(5), res.getDate(6),
+                      res.getString(7)));
+        return MailList;
     }
-
-    @Override
-    public List<Mail> getChallengeRequests(int userId) {
-        return null;
-    }
-
-    @Override
-    public List<Mail> getNotes(int userId) {
-        return null;
-    }
-
 
 }
